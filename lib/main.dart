ac91898752f3fd0de385642b8892fee4c1355ed4
tarboush63 <maleksaadi260@gmail.com>
2026-01-15 14:11:11 +0300

    fixed bugs, added notification handling, patched adding reviews to already reviewed apartments

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:simsar/Theme/app_theme.dart';
+import 'package:simsar/utils/error_snackbar.dart';
 import 'package:simsar/utils/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
@@ -26,9 +30,17 @@ class MyApp extends StatelessWidget {
       darkTheme: SAppTheme.darkTheme,
       themeMode: ThemeMode.system,
       routerConfig: AppRouter.router,
+      scaffoldMessengerKey: scaffoldMessengerKey,
     );
   }
 }
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Background notification: ${message.messageId}");
+}
+
 
 
 // End of the code
