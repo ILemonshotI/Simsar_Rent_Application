62418dc87752ea7a7c73165f2975ea37616c6772
tarboush63 <maleksaadi260@gmail.com>
2026-01-15 09:46:43 +0300

    firebase feature in progress, changed android files and added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:simsar/Screens/login_screen.dart';
 import 'package:simsar/Screens/register_screen.dart';
 import 'package:simsar/Theme/app_theme.dart';
 import 'package:simsar/utils/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
